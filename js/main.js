86bc37f253569b6dfa95a497b0d6c005ef0ea803
sal garcia <salgarcia940@gmail.com>
2021-12-03 14:39:04 -0800

    listened to the submit event and created a new obj and reset image preview also saved entried to the local storage by stringifying

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 /* global data */
 /* exported data */
+var $Title = document.querySelector('#title');
+var $photoUrl = document.querySelector('#photo-url');
+var $Notes = document.querySelector('#notes');
+var $image = document.querySelector('.image');
+
+var $form = document.querySelector('#form');
+
+$photoUrl.addEventListener('input', photoFunc);
+function photoFunc(event) {
+  $image.setAttribute('src', event.target.value);
+}
+
+$form.addEventListener('submit', formFunc);
+function formFunc(event) {
+  event.preventDefault();
+  var formInput = {
+    Title: $Title.value,
+    PhotoUrl: $photoUrl.value,
+    Notes: $Notes.value,
+    NextEntryId: data.nextEntryId
+  };
+  localStorage.setItem('Form Input Stringified', JSON.stringify(formInput));
+  data.entries.unshift(formInput);
+  data.nextEntryId++;
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
